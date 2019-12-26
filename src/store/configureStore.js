73fe47cca151cf4a0008ddca4fdaa7a954f6030e
Navneet <navneet@TM-ND0040.local>
2019-12-26 11:05:53 +0530

    updsted react devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,8 +3,8 @@ import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
-  const create = window.devToolsExtension ?
-    window.devToolsExtension()(createStore) :
+  const create = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION__()(createStore) :
     createStore;
 
   return applyMiddleware(thunk)(create)(rootReducer);
