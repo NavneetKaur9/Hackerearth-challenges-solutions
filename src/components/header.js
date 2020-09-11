ee32b5b24465644e3aeaa528b79b5c0b599f1fee
Navneet Pannu <pannu.navneet9@gmail.com>
2020-09-11 16:32:18 +0530

    code updated

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <nav className="navbar fixed-top navbar-dark bg-dark">
-                <span className="navbar-brand mb-0 h1"><i className="fas fa-camera-retro"></i>Insta - Image Sharing</span>
+                <span className="navbar-brand mb-0 h1"><i className="fas fa-camera-retro"></i> Insta - Image sharing</span>
             </nav>
         )
     }
